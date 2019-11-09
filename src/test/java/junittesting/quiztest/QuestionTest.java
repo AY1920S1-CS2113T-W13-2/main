916500aa8c8ed3f9eb3a34abe7fd270f21b187e4
claysmilesoil <e0323492.u.nus.edu>
2019-11-10 02:52:38 +0800

    checkstyle fixes

--- src/test/java/junittesting/quiztest/QuestionTest.java
+++ src/test/java/junittesting/quiztest/QuestionTest.java
@@ -4,7 +4,10 @@ import javacake.exceptions.CakeException;
 import javacake.quiz.Question;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class QuestionTest {
     Question dummyQuestion1 = new Question("abc", "3", 5);
@@ -22,10 +25,9 @@ public class QuestionTest {
 
     @Test
     public void inputValidationNotInRangeTest() {
-        Exception e1 = assertThrows(CakeException.class,
-                ()-> dummyQuestion1.isAnswerCorrect("0"));
+        Exception e1 = assertThrows(CakeException.class, () -> dummyQuestion1.isAnswerCorrect("0"));
         assertEquals(e1.getMessage(),
                 "[!] Please enter option number between 1 and 5! [!]\n");
-        assertThrows(CakeException.class, ()-> dummyQuestion1.isAnswerCorrect("6"));
+        assertThrows(CakeException.class, () -> dummyQuestion1.isAnswerCorrect("6"));
     }
 }
