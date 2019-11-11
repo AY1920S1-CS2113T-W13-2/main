e51cc02bcfc5ba40eaad8e1875a2c8bca68e8056
claysmilesoil <e0323492.u.nus.edu>
2019-11-11 19:19:24 +0800

    Tests for ReviewSession

--- src/test/java/junittesting/quiztest/QuestionTest.java
+++ src/test/java/junittesting/quiztest/QuestionTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class QuestionTest {
-    Question dummyQuestion1 = new Question("abc", "3", 5);
+    private Question dummyQuestion1 = new Question("abc", "3", 5);
 
     @Test
     public void inputValidationPositiveTest() throws CakeException {
